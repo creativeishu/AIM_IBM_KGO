8473e68e1d696eccd93552b3106d68cb1ce615de
Michele Dolfi <dolfim@phys.ethz.ch>
2014-10-27 22:43:41 +0100

    move phase to a node to add relations

--- simple_mat.py
+++ simple_mat.py
@@ -1,8 +1,11 @@
 import rdflib
+
+bname = 'simple_mat2'
+
 g = rdflib.Graph()
-result = g.parse("simple_mat.nt", format='nt')
+result = g.parse("%s.nt" % bname, format='nt')
 
 print "graph has %s statements." % len(g)
 
 import rdflib.tools.rdf2dot
-rdflib.tools.rdf2dot.rdf2dot(g, open('simple_mat.dot','w'))
+rdflib.tools.rdf2dot.rdf2dot(g, open("%s.dot" % bname,'w'))
