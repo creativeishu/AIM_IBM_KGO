815f4d7346b969815d84763f5376fb28fb66c2d4
Michele Dolfi <dolfim@phys.ethz.ch>
2014-10-27 22:33:58 +0100

    loading example with rdf: output not as expected

--- simple_mat.py
+++ simple_mat.py
@@ -1,3 +1,8 @@
 import rdflib
 g = rdflib.Graph()
-result = g.parse("simple_mat.nt")
+result = g.parse("simple_mat.nt", format='nt')
+
+print "graph has %s statements." % len(g)
+
+import rdflib.tools.rdf2dot
+rdflib.tools.rdf2dot.rdf2dot(g, open('simple_mat.dot','w'))
