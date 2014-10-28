78c04dd8c936a7b963b9c8c488eb2012e58050be
Ralph Koitz <ralph.koitz@gmail.com>
2014-10-28 12:23:03 +0100

    went back to simple wikipedia library.

--- main.py
+++ main.py
@@ -3,19 +3,29 @@
 
 import sys
 import json
-import pywikibot as pwb
-from   pywikibot import pagegenerators
+import wikipedia as wp
 
 
 def main(args):
     print "#getting periodic table"
     
-    site = pwb.Site()
-    pt_category = pwb.Category(site,'Category:Chemical elements')
+    output_to_file("test.txt",[["s","p","o"], ["x","y","z"]])
 
-    elements_gen = pwb.pagegenerators.CategorizedPageGenerator(pt_category)
+    periodic_table = wp.page("Category:Chemical element")
 
-    for el in  elements_gen:
-        print el.title()
+    print periodic_table.url
+
+    
+
+def output_to_file(filename, datalist):
+    outfile = open(filename, 'w')
+
+    outfile.write("[\n")
+    for line in datalist:
+        outfile.write("[ \"{0:s}\" , \"{1:s}\" , \"{2:s}\" ]\n".format(line[0],line[1],line[2]))
+
+    outfile.write("]\n")
+
+    outfile.close()
 
 main(sys.argv[1:])
