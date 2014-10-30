cdb7598b1afef780795dab7b3f61fbcddd8d8a9d
Ralph Koitz <ralph.koitz@gmail.com>
2014-10-30 13:05:38 +0100

    a few edges re-wired in the graph, iron, etc

--- scraper/rewire_graph.py
+++ scraper/rewire_graph.py
@@ -8,12 +8,28 @@ def main(args):
     prop_file = args[1]
 
     #list here :        bad id      good id
-    ids_to_replace = [('/m/02kcdj',           ),
-                                               ]
+    ids_to_replace = [("/m/02kctdj" ,"/m/025rsfk"),
+                      ("/m/0bnvbd4",  "/m/025rw19" ),
+                      ("/m/02kcv_c",  "/m/025rw19" ),
+                      ("/m/0bnv6p5",  "/m/025svlc" ),
+                      ("/m/02kcq04",  "/m/025s7j4" ),
+                      ("/m/02kcqln",  "/m/025sf9q" ),
+                      ("/m/063z0gk",  "/m/05_f5y5" ),
+                      ("/m/0640s9v",  "/m/05_f5zh" ),
+                      ("/m/08rbl0" ,  "/m/05_f5wn" ),
+                      ("/m/0ggk182",  "/m/0838f"   ),
+                      ("/m/02kcq1_",  "/m/025sf0_" ),
+                      ("/m/0ggk3fn",  "/m/06x4c"   ),
+                      ("/m/02kcr3f",  "/m/025tkrf" ),
+                      ("/m/02kcjvf",  "/m/025tkqy" )]
+
     bad_ids = [x[0] for x in ids_to_replace]
 
     edges = json.load(open(edge_file))
-    props = json.load(open(props_file))
+    props = json.load(open(prop_file))
+    assert (type(edges)==list)    
+    assert (type(props)==dict)    
+    print type(props)
 
     for edge in edges:
         for f,r in ids_to_replace:
