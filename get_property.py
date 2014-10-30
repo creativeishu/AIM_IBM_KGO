76e963044922876fd1819b86ea3fb005cb08b790
creativeishu <irshad@physik.uzh.ch>
2014-10-30 12:49:11 +0100

    ID added

--- get_property.py
+++ get_property.py
@@ -1,10 +1,65 @@
 #!/usr/bin/env python
 #This is the main tool to query/crawl wikipedia and output triples.
 
+import numpy
 import sys
-import wikipedia
+import time
+
+f = open('props_test.txt','r')
+outfile = open('props_test_new.txt','w')
+nodeIDS = []
+names = []
+i=0
+while True:
+    i+=1
+#    if (i%1000==0):
+#        print i
+    line = f.readline()
+    if not line:
+        break
+
+    if len(line)>10:
+        ID = (line.split(':')[0])
+        name = line.split('"name":')
+        if len(name)>1:
+            name = name[1]
+        else:
+            continue
+
+        name = name.split('"')[1]
+        if len(name)<2:
+            continue
+
+        if name == 'Uranium-231':
+            print "warning",ID,'Line:',i
 
 
+        if not name in names:
+            names.append(name)
+    #    else: 
+    #        name = name.split()
+    #        if len(name)==1:
+    #            print name
+    #            time.sleep(0.2)
+        nodeIDS.append(ID)
+
+f.close()
+outfile.close()
+print len(names),len(nodeIDS)
+
+sys.exit()
+
+for i in range(len(names)):
+    for j in range(i+1,len(names)):
+        if names[i]==names[j]:
+            print i,j,names[i],names[j]
+            time.sleep(0.2)
+
+
+
+
+
+'''
 class gethtml:
     def __init__(self,elementname):
         x = wikipedia.WikipediaPage(elementname)
@@ -30,13 +85,17 @@ class gethtml:
             print "Property not found"
             return None
 
-obj = gethtml("Iron")
 
-print obj.getproperty("Phase")
-print obj.getproperty("Element category")
-print obj.getproperty("Magnetic ordering")
-print obj.getproperty("Melting point")
+element = sys.argv[1]
 
+obj = gethtml(element)
+
+print element, obj.getproperty("Phase"),\
+                obj.getproperty("Element category"),\
+                obj.getproperty("Magnetic ordering"),\
+                obj.getproperty("Melting point")
+
+'''
 
 
 
