caaba33c58e2fbc1407e654280163b3eff25d50f
Michele Dolfi <dolfim@phys.ethz.ch>
2014-10-31 09:06:46 +0100

    change indentation according to Ilia's standards

--- database/daemon.cpp
+++ database/daemon.cpp
@@ -3,7 +3,7 @@
 #include "graph.hpp"
 
 
-// Path to the pipe used for communication (create with "mkfifo pipe_path" and set permissions right)
+  // Path to the pipe used for communication (create with "mkfifo pipe_path" and set permissions right)
 static const char* pipe_path = "/tmp/aim2014_graph_pipe";
 
 inline std::string trim(std::string& str)
@@ -15,37 +15,37 @@ inline std::string trim(std::string& str)
 
 int main(int argc, char* argv[])
 {
-    if(argc != 3)
-    {
-        std::cout << "Usage: "<< argv[0] << " <graph_file> <properties_file>" << std::endl;
-        return -1;
-    }
-    const std::string graph_file(argv[1]);
-    const std::string properties_file(argv[2]);
-    const graph G(graph_file,properties_file);
-
-    while(1)
-    {
-        // Get the query
-        bool by_name;
-        int depth;
-        std::string query;
-        std::ifstream ifs(pipe_path);
-        ifs >> by_name >> depth;
-        getline(ifs, query);
-        ifs.close();
-        trim(query);
-        std::cout << "By name  " << by_name << std::endl;
-        std::cout << "Depth    " << depth << std::endl;
-        std::cout << "Received " << query << std::endl;
-
-        // Reply to query
-        // const bool by_name(false);
-        const std::string res = G.query_graph(query,depth,by_name);
-        std::ofstream ofs(pipe_path);
-        ofs << res;
-        ofs.close();
-        std::cout << "Sent     " << res << std::endl;
-    }
-    return 0;
+  if(argc != 3)
+  {
+    std::cout << "Usage: "<< argv[0] << " <graph_file> <properties_file>" << std::endl;
+    return -1;
+  }
+  const std::string graph_file(argv[1]);
+  const std::string properties_file(argv[2]);
+  const graph G(graph_file,properties_file);
+  
+  while(1)
+  {
+      // Get the query
+    bool by_name;
+    int depth;
+    std::string query;
+    std::ifstream ifs(pipe_path);
+    ifs >> by_name >> depth;
+    getline(ifs, query);
+    ifs.close();
+    trim(query);
+    std::cout << "By name  " << by_name << std::endl;
+    std::cout << "Depth    " << depth << std::endl;
+    std::cout << "Received " << query << std::endl;
+    
+      // Reply to query
+      // const bool by_name(false);
+    const std::string res = G.query_graph(query,depth,by_name);
+    std::ofstream ofs(pipe_path);
+    ofs << res;
+    ofs.close();
+    std::cout << "Sent     " << res << std::endl;
+  }
+  return 0;
 }
